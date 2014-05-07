948959defaef8209dfd3fac99c84b8737a5fb70f
Stephen Tridgell <stephen@tridgell.net>
2014-05-07 15:30:10 +1000

    Added new display file

---
+++ damSim/display/runDisplay.java
@@ -0,0 +1,18 @@
+package display;
+
+import physicalObjects.*;
+
+/**
+ * This class opens a GUI to display the objects
+ */
+public class runDisplay {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
