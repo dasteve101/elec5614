c8761e705507652ceb81c082cf09cd53fe586c22
Stephen Tridgell <stephen@tridgell.net>
2014-05-08 13:32:07 +1000

    A few random ideas on the GUI

--- damSim/display/Gui.java
+++ damSim/display/Gui.java
@@ -1,13 +1,15 @@
 package display;
 
 import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Gui extends JFrame {
@@ -21,12 +23,22 @@ public class Gui extends JFrame {
 	public Gui() {
 		count = 0;
 		Container cp = getContentPane();
-		cp.setLayout(new FlowLayout());
+		cp.setLayout(new GridBagLayout());
 		cp.add(new JLabel("Counter"));
 		final JTextField tfCount = new JTextField("0", 10);
 		tfCount.setEditable(false);
 		cp.add(tfCount);
-
+		try{
+			final JPanel damImg = new ImagePanel("dam.jpg");
+			damImg.setSize(100, 100);
+			damImg.setLocation(200, 400);
+			cp.add(damImg);
+			System.out.println("Created");
+		}
+		catch( IOException e){
+			System.err.println("damImg could not be created");
+		}
+		
 		JButton btnCount = new JButton("Increment");
 		cp.add(btnCount);
 
@@ -44,7 +56,7 @@ public class Gui extends JFrame {
 														// close-window button
 														// clicked
 		setTitle("Dam Simulation"); // "this" JFrame sets title
-		setSize(300, 100); // "this" JFrame sets initial size
+		setSize(600, 400); // "this" JFrame sets initial size
 		setVisible(true); // "this" JFrame shows
 	}
 
