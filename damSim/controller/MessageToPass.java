b814525f4cde2cd1897be5592e9b87e077694b2d
Stephen Tridgell <stephen@tridgell.net>
2014-05-23 21:14:39 +1000

    Modified Dam thread to send/recieve msgs

--- damSim/controller/MessageToPass.java
+++ damSim/controller/MessageToPass.java
@@ -2,6 +2,9 @@ package controller;
 
 public class MessageToPass {
 	private float inflow;
+	private float capacity;
+	private float waterOut;
+	private float powerOut;
 	
 	public MessageToPass(float inflow){
 		this.inflow = inflow;
@@ -9,6 +12,30 @@ public class MessageToPass {
 	
 	public float getInflow(){
 		return inflow;
+	}
+
+	public float getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(float capacity) {
+		this.capacity = capacity;
+	}
+
+	public float getWaterOut() {
+		return waterOut;
+	}
+
+	public void setWaterOut(float waterOut) {
+		this.waterOut = waterOut;
+	}
+
+	public float getPowerOut() {
+		return powerOut;
+	}
+
+	public void setPowerOut(float powerOut) {
+		this.powerOut = powerOut;
 	}
 	
 }
